2ac4afe5e72c4529c8d083825eb3894f031955a1
Flojohhnn <juan.dominguez.rod@gmail.com>
2025-06-25 18:19:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ document.getElementById('email-form').addEventListener('submit', function (e) {
   e.preventDefault();
 
   const input = document.getElementById('email-input');
-  const email = input.value;
+  const email = input.value.trim();
   const confirmation = document.getElementById('confirmation-message');
   const form = document.getElementById('email-form');
 
   if (email) {
-    fetch(https://script.google.com/macros/s/AKfycbygITUcV2W1NVRS5UMIT6-aJrkdCREUxLZGTcWbPijMkYoxCpp9nP6FuSjZdMmtdQHiuQ/exec, {
+    fetch("https://script.google.com/macros/s/AKfycbygITUcV2W1NVRS5UMIT6-aJrkdCREUxLZGTcWbPijMkYoxCpp9nP6FuSjZdMmtdQHiuQ/exec", {
       method: "POST",
       mode: "no-cors",
       headers: {
